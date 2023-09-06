b546652ed9832071ded389eb0338bfc8531b3c2b
Tijana Savic <tijanasavic1984@gmail.com>
2023-09-06 18:50:10 +0200

    cleanup

--- src/helper.ts
+++ src/helper.ts
@@ -1,15 +1,15 @@
 export const removeDuplciates = (list: number[]) => {
-    const cache: { [key: string]: number } = {};
-    const result: number[] = [];
+  const cache: { [key: string]: number } = {};
+  const result: number[] = [];
 
-    list.forEach((item) => {
-        if (cache[`number-${item}`]) {
-            return;
-        } else {
-            cache[`number-${item}`] = item;
-            result.push(item); 
-        }
-    })
+  list.forEach((item) => {
+    if (cache[`number-${item}`]) {
+      return;
+    } else {
+      cache[`number-${item}`] = item;
+      result.push(item);
+    }
+  });
 
-    return result;
-}
+  return result;
+};
